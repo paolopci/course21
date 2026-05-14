a199e2ad6d8ac8606cf5030796e0d0884b599226
Paolo Paci <paolopci@yahoo.it>
2026-05-14 17:26:11 +0200

    @switch con select

--- src/app/section03/directive02/directive02.ts
+++ src/app/section03/directive02/directive02.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 interface empInterface {
   id: number;
@@ -10,12 +11,13 @@ interface empInterface {
 
 @Component({
   selector: 'app-directive02',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './directive02.html',
   styleUrl: './directive02.css',
 })
 export class Directive02 {
   //age: number = 210;
+  employeeRole: string = '';
 
   users = signal<empInterface[]>([
     { id: 1, nome: 'Mario', role: 'Developer', age: 10 },
@@ -23,5 +25,9 @@ export class Directive02 {
     { id: 3, nome: 'Peach', role: 'Designer', age: 30 },
     { id: 4, nome: 'gigi', role: 'Engineer', age: 40 },
     { id: 5, nome: 'Elisa', role: 'Deploy', age: 10 },
+    { id: 6, nome: 'Carla', role: 'HR', age: 10 },
+    { id: 7, nome: 'Franco', role: 'User', age: 50 },
+    { id: 8, nome: 'Celeste', role: 'Father', age: 80 },
+    { id: 9, nome: 'Venerina', role: 'Mom', age: 80 },
   ]);
 }
