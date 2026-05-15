826762928da9859838fa5eca491e6c61222211e4
Paolo Paci <paolopci@yahoo.it>
2026-05-15 10:50:30 +0200

    add App13 component with input handling and styles

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,8 @@
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { Directive02 } from './app/section03/directive02/directive02';
 import { config } from './app/app.config.server';
+import { App13 } from './app/section03/app13/app13';
 
-const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(Directive02, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(App13, config, context);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Directive01Component } from './app/section03/directive01.component/directive01.component';
 import { Directive02 } from './app/section03/directive02/directive02';
+import { App13 } from './app/section03/app13/app13';
 //bootstrapApplication(Directive01Component, appConfig).catch((err) => console.error(err));
-bootstrapApplication(Directive02, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App13, appConfig).catch((err) => console.error(err));
