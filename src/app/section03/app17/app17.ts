0f2464302b8ade3ed2cd8970465c7235fafe0224
Paolo Paci <paolopci@yahoo.it>
2026-05-15 11:39:13 +0200

    tag ng-container

---
+++ src/app/section03/app17/app17.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app17',
+  imports: [],
+  templateUrl: './app17.html',
+  styleUrl: './app17.css',
+})
+export class App17 {}
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { App } from './app/app';
 import { Directive01Component } from './app/section03/directive01.component/directive01.component';
 import { Directive02 } from './app/section03/directive02/directive02';
 import { App13 } from './app/section03/app13/app13';
+import { App17 } from './app/section03/app17/app17';
 //bootstrapApplication(Directive01Component, appConfig).catch((err) => console.error(err));
-bootstrapApplication(App13, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App17, appConfig).catch((err) => console.error(err));
